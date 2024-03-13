2d93c9a9589801d78cce99a327fc8df825317499
Emanuel Ionescu <emanuelionescu24@gmail.com>
2024-03-13 23:37:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import "./style.css";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  showSpinner();
+  const data = new FormData(form);
+
+  const response = await fetch(
+    "http://localhost:8080/",
+
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: data.get("prompt"),
+      }),
+    }
+  );
+
+  const { image } = await response.json();
+  const result = document.querySelector("#result");
+  result.innerHTML = `<img src="${image}" width="512"/>`;
+  hideSpinner();
+});
+
+function showSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = true;
+  button.innerHTML = "Se incarca...";
+}
+
+function hideSpinner() {
+  const button = document.querySelector("button");
+  button.disabled = false;
+  button.innerHTML = "Trimite";
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI,
+});
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const aiResponse = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    console.log("AI Response:", aiResponse); // raspunsul de la API
+    const image = aiResponse.data[0].url;
+    res.send({ image });
+  } catch (error) {
+    console.error("Error creating image:", error.message);
+    res.status(500).send("Error creating image");
+  }
+});
+
+app.listen(8080, () => console.log("Serverul a pornit"));
